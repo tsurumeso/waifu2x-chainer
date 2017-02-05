435091a9ff33e322464a69106f9c8e5f1fe54fdc
tomohiro <nstm101339@gmail.com>
2017-02-05 14:04:08 +0900

    Add support for Test-Time Augmentation

--- lib/reconstruct.py
+++ lib/reconstruct.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 from chainer import cuda
 
+from lib import iproc
 from lib import utils
 
 
@@ -47,8 +48,54 @@ def blockwise(model, src, block_size, batch_size):
             dst[:, ih:ih + block_size, jw:jw + block_size] = y[index]
 
     dst = dst[:, :h, :w]
-    dst = dst.transpose(1, 2, 0)
-    return np.clip(dst, 0, 1) * 255
+    return dst.transpose(1, 2, 0)
+
+
+def get_tta_patterns(src):
+    src_lr = src.transpose(Image.FLIP_LEFT_RIGHT)
+    patterns = [[src, None],
+               [src.transpose(Image.ROTATE_90), iproc.inv(-90)],
+               [src.transpose(Image.ROTATE_180), iproc.inv(-180)],
+               [src.transpose(Image.ROTATE_270), iproc.inv(-270)],
+               [src_lr, iproc.inv(0, True)],
+               [src_lr.transpose(Image.ROTATE_90), iproc.inv(-90, True)],
+               [src_lr.transpose(Image.ROTATE_180), iproc.inv(-180, True)],
+               [src_lr.transpose(Image.ROTATE_270), iproc.inv(-270, True)],
+    ]
+    return patterns 
+
+
+def scale_tta(model, src, block_size, batch_size):
+    patterns = get_tta_patterns(src)
+    dst = np.zeros((src.size[1] * 2, src.size[0] * 2, 3))
+    for i, (src, inv) in enumerate(patterns):
+        print(i, end=' ', flush=True)
+        src = src.resize((src.size[0] * 2, src.size[1] * 2), Image.NEAREST)
+        src = np.array(src.convert('RGB'), dtype=np.uint8)
+        tmp = blockwise(model, src, block_size, batch_size)
+        if not inv is None:
+            tmp = inv(tmp)
+        dst += tmp
+    dst /= len(patterns)
+    dst = np.clip(dst, 0, 1) * 255
+    dst = Image.fromarray(dst.astype(np.uint8))
+    return dst
+
+
+def noise_tta(model, src, block_size, batch_size):
+    patterns = get_tta_patterns(src)
+    dst = np.zeros((src.size[1], src.size[0], 3))
+    for i, (src, inv) in enumerate(patterns):
+        print(i, end=' ', flush=True)
+        src = np.array(src.convert('RGB'), dtype=np.uint8)
+        tmp = blockwise(model, src, block_size, batch_size)
+        if not inv is None:
+            tmp = inv(tmp)
+        dst += tmp
+    dst /= len(patterns)
+    dst = np.clip(dst, 0, 1) * 255
+    dst = Image.fromarray(dst.astype(np.uint8))
+    return dst
 
 
 def scale(model, src, block_size, batch_size):
@@ -56,12 +103,14 @@ def scale(model, src, block_size, batch_size):
     if model.ch == 1:
         src = np.array(src.convert('YCbCr'), dtype=np.uint8)
         dst = blockwise(model, src[:, :, 0], block_size, batch_size)
+        dst = np.clip(dst, 0, 1) * 255
         src[:, :, 0] = dst[:, :, 0]
         dst = Image.fromarray(src, mode='YCbCr').convert('RGB')
         return dst
     elif model.ch == 3:
         src = np.array(src.convert('RGB'), dtype=np.uint8)
         dst = blockwise(model, src, block_size, batch_size)
+        dst = np.clip(dst, 0, 1) * 255
         dst = Image.fromarray(dst.astype(np.uint8))
         return dst
 
@@ -70,11 +119,13 @@ def noise(model, src, block_size, batch_size):
     if model.ch == 1:
         src = np.array(src.convert('YCbCr'), dtype=np.uint8)
         dst = blockwise(model, src[:, :, 0], block_size, batch_size)
+        dst = np.clip(dst, 0, 1) * 255
         src[:, :, 0] = dst[:, :, 0]
         dst = Image.fromarray(src, mode='YCbCr').convert('RGB')
         return dst
     elif model.ch == 3:
         src = np.array(src.convert('RGB'), dtype=np.uint8)
         dst = blockwise(model, src, block_size, batch_size)
+        dst = np.clip(dst, 0, 1) * 255
         dst = Image.fromarray(dst.astype(np.uint8))
         return dst
