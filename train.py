97aeda2a947c44cdef91d9276c2c38c02f37cda1
tomohiro <nstm101339@gmail.com>
2017-02-09 03:40:15 +0900

    Fix memory leak in training

--- train.py
+++ train.py
@@ -20,26 +20,28 @@ from lib.loss.clipped_weighted_huber_loss import clipped_weighted_huber_loss
 def resampling(datalist, offset, cfg):
     insize = cfg.crop_size + offset
     sample_size = cfg.patches * len(datalist)
-    x = np.ndarray((sample_size, cfg.ch, insize, insize),
-                   dtype=np.float32)
-    y = np.ndarray((sample_size, cfg.ch, cfg.crop_size, cfg.crop_size),
-                   dtype=np.float32)
+    x = np.zeros((sample_size, cfg.ch, insize, insize),
+                 dtype=np.uint8)
+    y = np.zeros((sample_size, cfg.ch, cfg.crop_size, cfg.crop_size),
+                 dtype=np.uint8)
+
     for i in range(len(datalist)):
         img = iproc.read_image_rgb_uint8(datalist[i])
         xc_batch, yc_batch = pairwise_transform(img, insize, cfg)
         x[cfg.patches * i:cfg.patches * (i + 1)] = xc_batch[:]
         y[cfg.patches * i:cfg.patches * (i + 1)] = yc_batch[:]
-    return x * (1. / 255.), y * (1. / 255.)
+    return x, y
 
 
 def train_inner_epoch(model, optimizer, cfg, train_x, train_y):
     sum_loss = 0
+    scale = 1. / 255.
     xp = utils.get_model_module(model)
     perm = np.random.permutation(len(train_x))
     for i in range(0, len(train_x), cfg.batch_size):
         local_perm = perm[i:i + cfg.batch_size]
-        batch_x = xp.array(train_x[local_perm])
-        batch_y = xp.array(train_y[local_perm])
+        batch_x = xp.array(train_x[local_perm], dtype=np.float32) * scale
+        batch_y = xp.array(train_y[local_perm], dtype=np.float32) * scale
         if cfg.test:
             for j in range(0, len(batch_x)):
                 ix = iproc.to_image(batch_x[j], cfg.ch)
@@ -61,12 +63,13 @@ def train_inner_epoch(model, optimizer, cfg, train_x, train_y):
 
 def valid_inner_epoch(model, cfg, valid_x, valid_y):
     sum_score = 0
+    scale = 1. / 255.
     xp = utils.get_model_module(model)
     perm = np.random.permutation(len(valid_x))
     for i in range(0, len(valid_x), cfg.batch_size):
         local_perm = perm[i:i + cfg.batch_size]
-        batch_x = xp.array(valid_x[local_perm])
-        batch_y = xp.array(valid_y[local_perm])
+        batch_x = xp.array(valid_x[local_perm], dtype=np.float32) * scale
+        batch_y = xp.array(valid_y[local_perm], dtype=np.float32) * scale
         pred = model(batch_x)
         score = iproc.clipped_psnr(pred.data, batch_y)
         sum_score += score * len(batch_x)
@@ -74,13 +77,13 @@ def valid_inner_epoch(model, cfg, valid_x, valid_y):
 
 
 def train():
-    six.print_('* loading datalist...', end=' ')
+    six.print_('* loading datalist...', end=' ', flush=True)
     datalist = utils.load_datalist(args.dataset_dir)
     valid_num = int(np.ceil(args.validation_rate * len(datalist)))
     valid_list, train_list = datalist[:valid_num], datalist[valid_num:]
     six.print_('done')
 
-    six.print_('* loading model...', end=' ')
+    six.print_('* loading model...', end=' ', flush=True)
     if args.model_name is None:
         if args.method == 'noise':
             model_name = 'anime_style_noise%d_%s.npz' \
@@ -111,7 +114,7 @@ def train():
     train_config = copy.deepcopy(args)
     valid_config = copy.deepcopy(args)
 
-    six.print_('* sampling validation dataset...', end=' ')
+    six.print_('* sampling validation dataset...', end=' ', flush=True)
     valid_config.max_size = 0
     valid_config.active_cropping_rate = 1.0
     valid_config.patches = train_config.validation_crops
@@ -123,7 +126,7 @@ def train():
     best_loss = np.inf
     for epoch in range(0, train_config.epoch):
         six.print_('### epoch: %d ###' % epoch)
-        six.print_('  * resampling train dataset...', end=' ')
+        six.print_('  * resampling train dataset...', end=' ', flush=True)
         train_x, train_y = resampling(train_list, offset, train_config)
         six.print_('done')
         for inner_epoch in range(0, train_config.inner_epoch):
@@ -135,13 +138,14 @@ def train():
                                             valid_config, valid_x, valid_y)
             if train_loss < best_loss:
                 best_loss = train_loss
-                six.print_('    * best loss on train dataset: %f' % (train_loss))
+                six.print_('    * best loss on train dataset: %f'
+                           % train_loss)
             if valid_score > best_score:
                 best_count = 0
                 best_score = valid_score
                 six.print_('    * best score on validation dataset: PSNR %f dB'
-                    % (valid_score))
-                best_model = copy.deepcopy(model).to_cpu()
+                           % valid_score)
+                best_model = model.copy().to_cpu()
                 epoch_name = model_name.rstrip('.npz') + '_epoch%d.npz' % epoch
                 chainer.serializers.save_npz(epoch_name, best_model)
             if best_count >= train_config.lr_decay_interval:
@@ -150,7 +154,8 @@ def train():
                 if optimizer.alpha < train_config.lr_min:
                     optimizer.alpha = train_config.lr_min
                 else:
-                    six.print_('    * learning rate decay: %f' % (optimizer.alpha))
+                    six.print_('    * learning rate decay: %f'
+                               % optimizer.alpha)
 
 
 warnings.filterwarnings('ignore')
