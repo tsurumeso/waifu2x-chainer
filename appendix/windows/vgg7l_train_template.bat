940b62bdf5f1526865a7e0059b35fa4992fb63b0
tsurumeso <nstm101339@gmail.com>
2017-02-25 11:16:41 +0900

    Fix typo

--- appendix/windows/vgg7l_train_template.bat
+++ appendix/windows/vgg7l_train_template.bat
@@ -1,5 +1,5 @@
-python train.py --gpu 0 --dataset_dir jpeg_dataset --epoch 10 --model_name reference_scale_rgb --downsampling_filter box lanczos --lr_decay_interval 3
-python train.py --gpu 0 --dataset_dir png_dataset --finetune reference_scale_rgb.npz --downsampling_filter box lanczos
+python train.py --gpu 0 --dataset_dir jpeg_dataset --epoch 10 --model_name reference_scale_rgb --downsampling_filters box lanczos --lr_decay_interval 3
+python train.py --gpu 0 --dataset_dir png_dataset --finetune reference_scale_rgb.npz --downsampling_filters box lanczos
 python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 0 --finetune reference_scale_rgb.npz
 python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 1 --finetune reference_scale_rgb.npz
 python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 2 --finetune reference_scale_rgb.npz
