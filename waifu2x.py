89479aefad4f2a939fd681eacc5dab56db7815fa
tsurumeso <nstm101339@gmail.com>
2017-03-15 08:35:42 +0900

    Simplify options for compat

--- waifu2x.py
+++ waifu2x.py
@@ -16,11 +16,9 @@ def denoise_image(src, model, cfg):
     six.print_('Level %d denoising...' % cfg.noise_level, end=' ', flush=True)
     if cfg.tta:
         dst = reconstruct.image_tta(
-            src, model, False,
-            cfg.tta_level, cfg.block_size, cfg.batch_size)
+            src, model, False, cfg.tta_level, cfg.block_size, cfg.batch_size)
     else:
-        dst = reconstruct.image(
-            src, model, False, cfg.block_size, cfg.batch_size)
+        dst = reconstruct.image(src, model, cfg.block_size, cfg.batch_size)
     six.print_('OK')
     return dst
 
@@ -28,16 +26,14 @@ def denoise_image(src, model, cfg):
 def upscale_image(src, model, cfg):
     dst = src
     log_scale = np.log2(cfg.scale_factor)
-    upsampling = (model.inner_scale == 1)
     for _ in range(int(np.ceil(log_scale))):
         six.print_('2.0x upscaling...', end=' ', flush=True)
         if cfg.tta:
             dst = reconstruct.image_tta(
-                dst, model, upsampling,
+                dst, model, True,
                 cfg.tta_level, cfg.block_size, cfg.batch_size)
         else:
-            dst = reconstruct.image(
-                dst, model, upsampling, cfg.block_size, cfg.batch_size)
+            dst = reconstruct.image(dst, model, cfg.block_size, cfg.batch_size)
         six.print_('OK')
     if np.round(log_scale % 1.0, 6) != 0:
         six.print_('Resizing...', end=' ', flush=True)
@@ -50,7 +46,7 @@ def upscale_image(src, model, cfg):
 
 p = argparse.ArgumentParser()
 p.add_argument('--gpu', '-g', type=int, default=-1)
-p.add_argument('--input', '-i', default='images/small_noisy1.jpg')
+p.add_argument('--input', '-i', default='images/small.png')
 p.add_argument('--output', '-o', default='./')
 p.add_argument('--arch', '-a',
                choices=['VGG7', '0',
@@ -58,27 +54,21 @@ p.add_argument('--arch', '-a',
                         'ResNet10', '2',
                         'ResUpConv10', '3'],
                default='VGG7')
-p.add_argument('--model_dir', '-m', default=None)
-p.add_argument('--scale', '-s', action='store_true')
-p.add_argument('--scale_factor', '-S', type=float, default=2.0)
-p.add_argument('--noise', '-n', action='store_true')
-p.add_argument('--noise_scale', action='store_true')
-p.add_argument('--noise_level', '-N', type=int, choices=[0, 1, 2, 3],
+p.add_argument('--model_dir', '-d', default=None)
+p.add_argument('--method', '-m', choices=['noise', 'scale', 'noise_scale'],
+               default='scale')
+p.add_argument('--scale_factor', '-s', type=float, default=2.0)
+p.add_argument('--noise_level', '-n', type=int, choices=[0, 1, 2, 3],
                default=1)
 p.add_argument('--color', '-c', choices=['y', 'rgb'], default='rgb')
 p.add_argument('--tta', '-t', action='store_true')
 p.add_argument('--tta_level', '-T', type=int, choices=[2, 4, 8], default=8)
 p.add_argument('--block_size', type=int, default=64)
 p.add_argument('--batch_size', type=int, default=8)
-p.add_argument('--test', action='store_true')
 
 args = p.parse_args()
 if args.arch in srcnn.table:
     args.arch = srcnn.table[args.arch]
-if args.test:
-    args.scale = True
-    args.noise = True
-    args.noise_level = 1
 formats = ['.png', '.jpg', '.jpeg', '.bmp', '.gif']
 
 if __name__ == '__main__':
@@ -90,32 +80,34 @@ if __name__ == '__main__':
     if not os.path.exists(args.output):
         os.makedirs(args.output)
 
-    if args.noise_scale:
+    models = {}
+    flag = False
+    if args.method == 'noise_scale':
         model_name = ('anime_style_noise%d_scale_%s.npz'
                       % (args.noise_level, args.color))
         model_path = os.path.join(model_dir, model_name)
-        model_noise_scale = srcnn.archs[args.arch](ch)
-        chainer.serializers.load_npz(model_path, model_noise_scale)
-    else:
-        if args.scale:
-            model_name = 'anime_style_scale_%s.npz' % args.color
-            model_path = os.path.join(model_dir, model_name)
-            model_scale = srcnn.archs[args.arch](ch)
-            chainer.serializers.load_npz(model_path, model_scale)
-        if args.noise:
-            model_name = ('anime_style_noise%d_%s.npz'
-                          % (args.noise_level, args.color))
-            model_path = os.path.join(model_dir, model_name)
-            model_noise = srcnn.archs[args.arch](ch)
-            chainer.serializers.load_npz(model_path, model_noise)
+        if os.path.exists(model_path):
+            models['noise_scale'] = srcnn.archs[args.arch](ch)
+            chainer.serializers.load_npz(model_path, models['noise_scale'])
+        else:
+            flag = True
+    if args.method == 'scale' or flag:
+        model_name = 'anime_style_scale_%s.npz' % args.color
+        model_path = os.path.join(model_dir, model_name)
+        models['scale'] = srcnn.archs[args.arch](ch)
+        chainer.serializers.load_npz(model_path, models['scale'])
+    if args.method == 'noise' or flag:
+        model_name = ('anime_style_noise%d_%s.npz'
+                      % (args.noise_level, args.color))
+        model_path = os.path.join(model_dir, model_name)
+        models['noise'] = srcnn.archs[args.arch](ch)
+        chainer.serializers.load_npz(model_path, models['noise'])
 
     if args.gpu >= 0:
         cuda.check_cuda_available()
         cuda.get_device(args.gpu).use()
-        if args.scale:
-            model_scale.to_gpu()
-        if args.noise:
-            model_noise.to_gpu()
+        for _, model in models.items():
+            model.to_gpu()
 
     if os.path.isdir(args.input):
         filelist = glob.glob(os.path.join(args.input, '*.*'))
@@ -131,16 +123,16 @@ if __name__ == '__main__':
             oname += ('_(tta%d)' % args.tta_level if args.tta else '_')
 
             dst = src.copy()
-            if args.noise_scale:
+            if 'noise_scale' in models:
                 oname += '(noise%d_scale)' % args.noise_level
-                dst = upscale_image(dst, model_noise_scale, args)
+                dst = upscale_image(dst, models['noise_scale'], args)
             else:
-                if args.noise:
+                if 'noise' in models:
                     oname += '(noise%d)' % args.noise_level
-                    dst = denoise_image(dst, model_noise, args)
-                if args.scale:
+                    dst = denoise_image(dst, models['noise'], args)
+                if 'scale' in models:
                     oname += '(scale%.1fx)' % args.scale_factor
-                    dst = upscale_image(dst, model_scale, args)
+                    dst = upscale_image(dst, models['scale'], args)
 
             if args.model_dir is None:
                 oname += '(%s_%s).png' % (args.arch.lower(), args.color)
