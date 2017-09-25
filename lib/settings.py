066b005ec440e217c2743d5a4c9a4a9d6a0781f1
tsurumeso <nstm101339@gmail.com>
2017-09-26 01:09:58 +0900

    Simplify some options

--- lib/settings.py
+++ lib/settings.py
@@ -5,20 +5,21 @@ from lib import srcnn
 
 p = argparse.ArgumentParser(
     description='Chainer implementation of waifu2x model trainer')
-p.add_argument('--gpu', type=int, default=-1)
+p.add_argument('--gpu', '-g', type=int, default=-1)
 p.add_argument('--seed', type=int, default=11)
-p.add_argument('--dataset_dir', required=True)
+p.add_argument('--dataset_dir', '-d', required=True)
 p.add_argument('--validation_rate', type=float, default=0.05)
-p.add_argument('--color', choices=['y', 'rgb'], default='rgb')
-p.add_argument('--arch',
+p.add_argument('--color', '-c', choices=['y', 'rgb'], default='rgb')
+p.add_argument('--arch', '-a',
                choices=['VGG7', '0',
                         'UpConv7', '1',
                         'ResNet10', '2',
                         'ResUpConv10', '3'],
                default='VGG7')
-p.add_argument('--method', choices=['noise', 'scale', 'noise_scale'],
+p.add_argument('--method', '-m', choices=['noise', 'scale', 'noise_scale'],
                default='scale')
-p.add_argument('--noise_level', type=int, choices=[0, 1, 2, 3], default=1)
+p.add_argument('--noise_level', '-n', type=int, choices=[0, 1, 2, 3],
+               default=1)
 p.add_argument('--nr_rate', type=float, default=0.65)
 p.add_argument('--chroma_subsampling_rate', type=float, default=0.5)
 p.add_argument('--out_size', type=int, default=64)
@@ -32,15 +33,15 @@ p.add_argument('--learning_rate', type=float, default=0.00025)
 p.add_argument('--lr_min', type=float, default=0.00001)
 p.add_argument('--lr_decay', type=float, default=0.9)
 p.add_argument('--lr_decay_interval', type=int, default=5)
-p.add_argument('--batch_size', type=int, default=16)
-p.add_argument('--patches', type=int, default=64)
+p.add_argument('--batch_size', '-b', type=int, default=16)
+p.add_argument('--patches', '-p', type=int, default=64)
 p.add_argument('--validation_crop_rate', type=float, default=0.5)
 p.add_argument('--downsampling_filters', nargs='+', default=['box'])
 p.add_argument('--resize_blur_min', type=float, default=0.95)
 p.add_argument('--resize_blur_max', type=float, default=1.05)
-p.add_argument('--epoch', type=int, default=50)
+p.add_argument('--epoch', '-e', type=int, default=50)
 p.add_argument('--inner_epoch', type=int, default=4)
-p.add_argument('--finetune', default=None)
+p.add_argument('--finetune', '-f', default=None)
 p.add_argument('--model_name', default=None)
 
 args = p.parse_args()
--- waifu2x.py
+++ waifu2x.py
@@ -62,12 +62,12 @@ p.add_argument('--scale_factor', '-s', type=float, default=2.0)
 p.add_argument('--noise_level', '-n', type=int, choices=[0, 1, 2, 3],
                default=1)
 p.add_argument('--color', '-c', choices=['y', 'rgb'], default='rgb')
-p.add_argument('--tta', '-t', action='store_true')
-p.add_argument('--tta_level', '-T', type=int, choices=[2, 4, 8], default=8)
-p.add_argument('--block_size', type=int, default=64)
-p.add_argument('--batch_size', type=int, default=8)
-p.add_argument('--width', type=int, default=0)
-p.add_argument('--height', type=int, default=0)
+p.add_argument('--tta', action='store_true')
+p.add_argument('--tta_level', type=int, choices=[2, 4, 8], default=8)
+p.add_argument('--batch_size', '-b', type=int, default=8)
+p.add_argument('--block_size', '-l', type=int, default=64)
+p.add_argument('--width', '-W', type=int, default=0)
+p.add_argument('--height', '-H', type=int, default=0)
 
 args = p.parse_args()
 if args.arch in srcnn.table:
