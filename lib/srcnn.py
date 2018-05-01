b8110fb98da2a48a2c4fd6b2607950f6cafc56e9
tsurumeso <tsurumeso@gmail.com>
2018-05-01 18:41:57 +0900

    Fix to use init_scope()

--- lib/srcnn.py
+++ lib/srcnn.py
@@ -6,70 +6,74 @@ import chainer.links as L
 class VGG7(chainer.Chain):
 
     def __init__(self, ch):
+        super(VGG7, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(ch, 32, 3)
+            self.conv2 = L.Convolution2D(32, 32, 3)
+            self.conv3 = L.Convolution2D(32, 64, 3)
+            self.conv4 = L.Convolution2D(64, 64, 3)
+            self.conv5 = L.Convolution2D(64, 128, 3)
+            self.conv6 = L.Convolution2D(128, 128, 3)
+            self.conv7 = L.Convolution2D(128, ch, 3)
+
         self.ch = ch
         self.offset = 7
         self.inner_scale = 1
-        super(VGG7, self).__init__(
-            conv0=L.Convolution2D(ch, 32, 3),
-            conv1=L.Convolution2D(32, 32, 3),
-            conv2=L.Convolution2D(32, 64, 3),
-            conv3=L.Convolution2D(64, 64, 3),
-            conv4=L.Convolution2D(64, 128, 3),
-            conv5=L.Convolution2D(128, 128, 3),
-            conv6=L.Convolution2D(128, ch, 3),
-        )
 
     def __call__(self, x):
-        h = F.leaky_relu(self.conv0(x), 0.1)
-        h = F.leaky_relu(self.conv1(h), 0.1)
+        h = F.leaky_relu(self.conv1(x), 0.1)
         h = F.leaky_relu(self.conv2(h), 0.1)
         h = F.leaky_relu(self.conv3(h), 0.1)
         h = F.leaky_relu(self.conv4(h), 0.1)
         h = F.leaky_relu(self.conv5(h), 0.1)
-        y = self.conv6(h)
-        return y
+        h = F.leaky_relu(self.conv6(h), 0.1)
+        h = self.conv7(h)
+        return h
 
 
 class UpConv7(chainer.Chain):
 
     def __init__(self, ch):
+        super(UpConv7, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(ch, 16, 3)
+            self.conv2 = L.Convolution2D(16, 32, 3)
+            self.conv3 = L.Convolution2D(32, 64, 3)
+            self.conv4 = L.Convolution2D(64, 128, 3)
+            self.conv5 = L.Convolution2D(128, 256, 3)
+            self.conv6 = L.Convolution2D(256, 256, 3)
+            self.conv7 = L.Deconvolution2D(256, ch, 4, 2, 3, nobias=True)
+
         self.ch = ch
         self.offset = 14
         self.inner_scale = 2
-        super(UpConv7, self).__init__(
-            conv0=L.Convolution2D(ch, 16, 3),
-            conv1=L.Convolution2D(16, 32, 3),
-            conv2=L.Convolution2D(32, 64, 3),
-            conv3=L.Convolution2D(64, 128, 3),
-            conv4=L.Convolution2D(128, 256, 3),
-            conv5=L.Convolution2D(256, 256, 3),
-            conv6=L.Deconvolution2D(256, ch, 4, 2, 3, nobias=True),
-        )
 
     def __call__(self, x):
-        h = F.leaky_relu(self.conv0(x), 0.1)
-        h = F.leaky_relu(self.conv1(h), 0.1)
+        h = F.leaky_relu(self.conv1(x), 0.1)
         h = F.leaky_relu(self.conv2(h), 0.1)
         h = F.leaky_relu(self.conv3(h), 0.1)
         h = F.leaky_relu(self.conv4(h), 0.1)
         h = F.leaky_relu(self.conv5(h), 0.1)
-        y = self.conv6(h)
-        return y
+        h = F.leaky_relu(self.conv6(h), 0.1)
+        h = self.conv7(h)
+        return h
 
 
 class ResBlock(chainer.Chain):
 
     def __init__(self, in_channels, out_channels, slope=0.1):
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.slope = slope
-        super(ResBlock, self).__init__(
-            conv1=L.Convolution2D(in_channels, out_channels, 3),
-            conv2=L.Convolution2D(out_channels, out_channels, 3),
-        )
+        super(ResBlock, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(in_channels, out_channels, 3)
+            self.conv2 = L.Convolution2D(out_channels, out_channels, 3)
+
         if in_channels != out_channels:
             conv_bridge = L.Convolution2D(in_channels, out_channels, 1)
             self.add_link('conv_bridge', conv_bridge)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.slope = slope
 
     def __call__(self, x):
         h = F.leaky_relu(self.conv1(x), self.slope)
@@ -90,30 +94,31 @@ class ResNet10(chainer.Chain):
     """
 
     def __init__(self, ch):
+        super(ResNet10, self).__init__()
+        with self.init_scope():
+            self.conv_pre = L.Convolution2D(ch, 64, 3)
+            self.res1 = ResBlock(64, 64)
+            self.res2 = ResBlock(64, 64)
+            self.res3 = ResBlock(64, 64)
+            self.res4 = ResBlock(64, 64)
+            self.res5 = ResBlock(64, 64)
+            self.conv_bridge = L.Convolution2D(64, 64, 3)
+            self.conv_post = L.Convolution2D(64, ch, 3)
+
         self.ch = ch
         self.offset = 13
         self.inner_scale = 1
-        super(ResNet10, self).__init__(
-            conv_fe=L.Convolution2D(ch, 64, 3),
-            res1=ResBlock(64, 64),
-            res2=ResBlock(64, 64),
-            res3=ResBlock(64, 64),
-            res4=ResBlock(64, 64),
-            res5=ResBlock(64, 64),
-            conv6=L.Convolution2D(64, 64, 3),
-            conv_be=L.Convolution2D(64, ch, 3),
-        )
 
     def __call__(self, x):
-        h = skip = F.leaky_relu(self.conv_fe(x), 0.1)
+        h = skip = F.leaky_relu(self.conv_pre(x), 0.1)
         h = self.res1(h)
         h = self.res2(h)
         h = self.res3(h)
         h = self.res4(h)
         h = self.res5(h)
-        h = F.leaky_relu(self.conv6(h), 0.1)
+        h = F.leaky_relu(self.conv_bridge(h), 0.1)
         h = h + skip[:, :, 11:-11, 11:-11]
-        h = self.conv_be(h)
+        h = self.conv_post(h)
         return h
 
 
