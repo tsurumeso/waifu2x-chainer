1f76a483edbe330588602fa584cfed6cdd150cf9
tsurumeso <tsurumeso@gmail.com>
2018-10-03 21:45:36 +0900

    Rename ResNet10 to UpResNet10 and improve the architecture

--- lib/srcnn.py
+++ lib/srcnn.py
@@ -85,26 +85,6 @@ class ResBlock(chainer.Chain):
         return h + x
 
 
-class UpsampleBlock(chainer.Chain):
-
-    def __init__(self, in_channels, out_channels, r=2, slope=0.1):
-        super(UpsampleBlock, self).__init__()
-        mid_channels = in_channels * r ** 2
-        with self.init_scope():
-            self.conv1 = L.Convolution2D(in_channels, mid_channels, 3)
-            self.conv2 = L.Convolution2D(in_channels, in_channels, 3)
-            self.conv3 = L.Convolution2D(in_channels, out_channels, 3)
-
-        self.r = r
-        self.slope = slope
-
-    def __call__(self, x):
-        h = F.depth2space(self.conv1(x), self.r)
-        h = F.leaky_relu(self.conv2(h), self.slope)
-        h = self.conv3(h)
-        return h
-
-
 class ResNet10(chainer.Chain):
 
     def __init__(self, ch):
@@ -116,11 +96,42 @@ class ResNet10(chainer.Chain):
             self.res3 = ResBlock(64, 64)
             self.res4 = ResBlock(64, 64)
             self.res5 = ResBlock(64, 64)
-            self.conv6 = L.Convolution2D(64, 64, 3)
-            self.upsample = UpsampleBlock(64, ch)
+            self.conv_bridge = L.Convolution2D(64, 64, 3)
+            self.conv_post = L.Convolution2D(64, ch, 3)
 
         self.ch = ch
-        self.offset = 28
+        self.offset = 13
+        self.inner_scale = 1
+
+    def __call__(self, x):
+        h = skip = F.leaky_relu(self.conv_pre(x), 0.1)
+        h = self.res1(h)
+        h = self.res2(h)
+        h = self.res3(h)
+        h = self.res4(h)
+        h = self.res5(h)
+        h = F.leaky_relu(self.conv_bridge(h), 0.1)
+        h = h + skip[:, :, 11:-11, 11:-11]
+        h = self.conv_post(h)
+        return h
+
+
+class UpResNet10(chainer.Chain):
+
+    def __init__(self, ch):
+        super(UpResNet10, self).__init__()
+        with self.init_scope():
+            self.conv_pre = L.Convolution2D(ch, 64, 3)
+            self.res1 = ResBlock(64, 64)
+            self.res2 = ResBlock(64, 64)
+            self.res3 = ResBlock(64, 64)
+            self.res4 = ResBlock(64, 64)
+            self.res5 = ResBlock(64, 64)
+            self.conv_bridge = L.Convolution2D(64, 64, 3)
+            self.conv_post = L.Deconvolution2D(64, ch, 4, 2, 3, nobias=True)
+
+        self.ch = ch
+        self.offset = 26
         self.inner_scale = 2
 
     def __call__(self, x):
@@ -130,9 +141,9 @@ class ResNet10(chainer.Chain):
         h = self.res3(h)
         h = self.res4(h)
         h = self.res5(h)
-        h = F.leaky_relu(self.conv6(h), 0.1)
+        h = F.leaky_relu(self.conv_bridge(h), 0.1)
         h = h + skip[:, :, 11:-11, 11:-11]
-        h = self.upsample(h)
+        h = self.conv_post(h)
         return h
 
 
@@ -140,10 +151,12 @@ archs = {
     'VGG7': VGG7,
     'UpConv7': UpConv7,
     'ResNet10': ResNet10,
+    'UpResNet10': UpResNet10,
 }
 
 table = {
     '0': 'VGG7',
     '1': 'UpConv7',
     '2': 'ResNet10',
+    '3': 'UpResNet10',
 }
