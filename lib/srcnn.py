2d70dad1ee756e1b4b1b600eaec162a74597e19c
tsurumeso <tsurumeso@gmail.com>
2018-10-17 00:10:06 +0900

    Improve UpResNet10 architecture

--- lib/srcnn.py
+++ lib/srcnn.py
@@ -116,17 +116,47 @@ class ResNet10(chainer.Chain):
         return h
 
 
+class SEResBlock(chainer.Chain):
+
+    def __init__(self, in_channels, out_channels, r=16, slope=0.1):
+        super(SEResBlock, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(in_channels, out_channels, 3)
+            self.conv2 = L.Convolution2D(out_channels, out_channels, 3)
+            self.fc1 = L.Linear(out_channels, out_channels // r)
+            self.fc2 = L.Linear(out_channels // r, out_channels)
+
+        if in_channels != out_channels:
+            conv_bridge = L.Convolution2D(in_channels, out_channels, 1)
+            self.add_link('conv_bridge', conv_bridge)
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.slope = slope
+
+    def __call__(self, x):
+        h = F.leaky_relu(self.conv1(x), self.slope)
+        h = F.leaky_relu(self.conv2(h), self.slope)
+        se = F.relu(self.fc1(_global_average_pooling_2d(h)))
+        se = F.sigmoid(self.fc2(se))[:, :, None, None]
+        if self.in_channels != self.out_channels:
+            x = self.conv_bridge(x[:, :, 2:-2, 2:-2])
+        else:
+            x = x[:, :, 2:-2, 2:-2]
+        return h * se + x
+
+
 class UpResNet10(chainer.Chain):
 
     def __init__(self, ch):
         super(UpResNet10, self).__init__()
         with self.init_scope():
             self.conv_pre = L.Convolution2D(ch, 64, 3)
-            self.res1 = ResBlock(64, 64)
-            self.res2 = ResBlock(64, 64)
-            self.res3 = ResBlock(64, 64)
-            self.res4 = ResBlock(64, 64)
-            self.res5 = ResBlock(64, 64)
+            self.res1 = SEResBlock(64, 64, 4)
+            self.res2 = SEResBlock(64, 64, 4)
+            self.res3 = SEResBlock(64, 64, 4)
+            self.res4 = SEResBlock(64, 64, 4)
+            self.res5 = SEResBlock(64, 64, 4)
             self.conv_bridge = L.Convolution2D(64, 64, 3)
             self.conv_post = L.Deconvolution2D(64, ch, 4, 2, 3, nobias=True)
 
@@ -145,6 +175,12 @@ class UpResNet10(chainer.Chain):
         h = h + skip[:, :, 11:-11, 11:-11]
         h = self.conv_post(h)
         return h
+
+
+def _global_average_pooling_2d(x):
+    n, channel, rows, cols = x.data.shape
+    h = F.average_pooling_2d(x, (rows, cols), stride=1)
+    return h
 
 
 archs = {
