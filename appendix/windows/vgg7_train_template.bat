3202cb317fbd735e3df3f2e79c8e61ca79d6dd8a
tsurumeso <tsurumeso@gmail.com>
2018-10-23 21:34:11 +0900

    Modify template script of VGG7

--- appendix/windows/vgg7_train_template.bat
+++ appendix/windows/vgg7_train_template.bat
@@ -1,7 +1,7 @@
 python train.py --gpu 0 --dataset_dir jpeg_dataset --epoch 10 --patches 32 --model_name reference_scale_rgb --downsampling_filters box lanczos --lr_decay_interval 3
 python train.py --gpu 0 --dataset_dir png_dataset --finetune reference_scale_rgb.npz --downsampling_filters box lanczos
-python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 0 --finetune anime_style_scale_rgb.npz
-python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 1 --finetune anime_style_scale_rgb.npz
-python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 2 --finetune anime_style_scale_rgb.npz
-python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 3 --finetune anime_style_scale_rgb.npz --nr_rate 1.0
+python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 0 --finetune reference_scale_rgb.npz
+python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 1 --finetune reference_scale_rgb.npz
+python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 2 --finetune reference_scale_rgb.npz
+python train.py --gpu 0 --dataset_dir png_dataset --method noise --noise_level 3 --finetune reference_scale_rgb.npz --nr_rate 1.0
 pause
